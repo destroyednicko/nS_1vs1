664e3e642409d5d8f487fa5e16656645a69b8864
Extry <45901764+Extry@users.noreply.github.com>
2019-09-15 20:25:09 -0300

    Update BuildCMD.java

--- nS_1vs1/src/kun/nicko/cmds/BuildCMD.java
+++ nS_1vs1/src/kun/nicko/cmds/BuildCMD.java
@@ -5,25 +5,30 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-
+import org.bukkit.command.Command;
 import kun.nicko.Main;
 import kun.nicko.Message;
 
+public class BuildCMD extends Command{
+	
+		public BuildCMD() {
+		super("build");
 
-public class BuildCMD implements CommandExecutor{
+	}
+	
 	
 	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	public boolean execute(CommandSender s, String arg1, String[] args) {
 		if (sender.hasPermission("lobby.gerente") || sender.hasPermission("lobby.builder")) {
 			if (sender instanceof Player) {
 				if (args.length <= 1) {
 					if (Main.build.contains(sender)) {
-						sender.sendMessage(Main.prefix + "§7Você não pode mais construir.");
+						sender.sendMessage(Main.prefix + "Â§7VocÃª nÃ£o pode mais construir.");
 						Message.failsound((Player)sender);
 						Main.build.remove((Player) sender);
 						((Player) sender).setGameMode(GameMode.SURVIVAL);
 					} else {
-						sender.sendMessage(Main.prefix + "§7Agora você pode construir.");
+						sender.sendMessage(Main.prefix + "Â§7Agora vocÃª pode construir.");
 						Message.sucesssound((Player)sender);
 						Main.build.add((Player) sender);
 						((Player) sender).setGameMode(GameMode.CREATIVE);
